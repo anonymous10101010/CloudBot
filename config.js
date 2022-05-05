40873bd886e6fc0bb22f20ac077f069c2b8c7905
anonymous10101010 <102923293+anonymous10101010@users.noreply.github.com>
2022-05-05 15:46:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281224863098'],
+  ['972557216809'],
   ['0'],
-  ['6281224863098', 'Della', true]
+  ['972557216809', 'Della', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'Created By'
-global.author = 'Cloud Bot'
+global.author = 'LUCII'
 
 global.multiplier = 69 // The higher, The harder levelup
 
